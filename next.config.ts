e1fac0c3ae231b71ba78ecb0f338255545991de1
wendt-apst <wendt@smbapps.com.au>
2026-08-16 13:34:35 +0530

    Add dockerfile

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['172.31.240.1', 'localhost'],
+  output: "standalone",
 };
 
 export default nextConfig;
